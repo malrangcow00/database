8cf3a7f573491877beeef3cf06ad492d97c80597
JP <115908997+malrangcow00@users.noreply.github.com>
2024-03-04 17:58:39 +0900

    0304_TIL

---
+++ DCL.sql
@@ -0,0 +1,65 @@
+# DCL(Data Control Language, 데이터 제어어)
+
+
+# USER 관련 명령어
+
+    # CREATE USER
+    # 사용자를 생성하는 명령어
+    CREATE USER username IDENTIFIED BY password;
+
+    # ALTER USER
+    # 사용자의 정보를 변경하는 명령어
+    ALTER USER username IDENTIFIED BY password;
+
+    # DROP USER
+    # 사용자를 삭제하는 명령어
+    DROP USER username;
+
+
+# 권한 관련 명령어
+
+    # GRANT
+    # 사용자에게 권한을 부여하는 명령어
+    GRANT authority TO username;
+
+    # REVOKE
+    # 사용자에게 부여된 권한을 회수하는 명령어
+    REVOKE authority FROM username;
+
+    # Authorities
+    # 권한 목록
+    # CONNECT : 데이터베이스에 접속할 수 있는 권한
+    # RESOURCE : 테이블, 시퀀스, 프로시저 등을 생성할 수 있는 권한
+    # DBA : 데이터베이스 관리자 권한
+
+
+
+
+
+
+CREATE USER CHOONSIK IDENTIFIED BY ssafy0000;
+
+GRANT DBA TO CHOONSIK;
+
+CREATE TABLE TEST (COL1 VARCHAR2(1));
+
+INSERT INTO TEST VALUES ('A');
+
+SELECT * FROM TEST;
+
+CREATE TABLE USER (
+      ID INT PRIMARY KEY,
+      NAME VARCHAR(100),
+      EMAIL VARCHAR(100)
+);
+
+INSERT INTO USER (ID, NAME, EMAIL) VALUES (1, 'CHOONSIK', 'gumissafy00@gmail.com');
+
+# 열 추가
+ALTER TABLE USER ADD AGE INT;
+
+# 테이블에서 3열인 EMAIL와 4열인 AGE의 열 순서 변경
+ALTER TABLE USER MODIFY EMAIL VARCHAR(100) FIRST;
+ALTER TABLE USER MODIFY EMAIL VARCHAR(100) AFTER AGE;
+ALTER TABLE USER MODIFY AGE INT AFTER EMAIL;
+
--- README.md
+++ README.md
@@ -1,1 +1,31 @@
-# Structured-Query-Language
+```bash
+# install ipython-sql
+$ pip install ipython-sql
+
+# install SQL server
+$ pip install pyodbc
+
+# install MySQL
+$ pip install PyMySQL
+
+# install PostgreSQL
+$ pip install psycopg2
+```
+
+or
+
+```bash
+$ pip install -r requirements.txt
+```
+
+connect to the database using the following codes:
+```python
+# SQL Server
+%sql mssql+pyodbc://user_name:password@host:port_number/db
+
+# PostgreSQL
+%sql postgresql://user_name:password@host:port_number/db
+            
+# MySQL
+%sql mysql://user_name:password@host:port_number/db
+```
